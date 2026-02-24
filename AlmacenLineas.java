95d1118bd61d79e3a7e19cb70035e046e8fe4324
marcoaranda02 <marco.as@culiacan.tecnm.mx>
2026-02-23 17:48:35 -0700

    first commit

---
+++ AlmacenLineas.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AlmacenLineas {
+    //el almacen tiene un arreglo de lineas, así como un arreglo de interesados
+        private List<String> lineas = new ArrayList<>();
+        private List<EscuchadorDeDatos> interesados = new ArrayList<>();
+    //el metodo suscribirse recibiria objetos que compartan la interfaz comun escuchadorDeDatos y serán agregados a la lista
+        public void suscribir(EscuchadorDeDatos modulo) {
+            interesados.add(modulo);
+        }
+    //agrega una al almacen y se le "grita" que ha habido cambios y que los interesados deben reaccionar
+        public void agregarLinea(String linea) {
+            lineas.add(linea);
+            // Aquí es donde "grita"
+            for (EscuchadorDeDatos e : interesados) {
+                e.reaccionarNuevoCambio(); 
+            }
+        }
+    //  obtiene la ultima linea agregada, que es la que se le indica a los interesados
+        public String obtenerUltima() {
+            return lineas.get(lineas.size() - 1);
+        }
+    
+}
